fd746c11584b4e8c8f607219825cdca42a6fd9ac
Alexander May <guitarx3@gmail.com>
2019-11-11 22:29:19 +0100

    tailwind + next

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import "../styles/index.css";
+
+export default () => (
+  <div className="p-4 shadow rounded bg-white">
+    <h1 className="text-purple-500 leading-normal">Next.js</h1>
+    <p className="text-gray-500">with Tailwind CSS</p>
+  </div>
+);
