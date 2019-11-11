ae3d05b75aa2010e4e631bdf0dc42dd6f6afb88d
Alexander May <guitarx3@gmail.com>
2019-11-11 22:48:33 +0100

    add purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./pages/**/*.tsx",
+    "./styles/**/*.css"
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("postcss-preset-env")]
+  plugins: [require("tailwindcss"), require("postcss-preset-env"), purgecss]
 };
