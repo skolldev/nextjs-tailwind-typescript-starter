b19a02dcfb4e9e0a14d5e936c4923137a84ee67a
xDecus <guitarx3@gmail.com>
2019-11-12 17:15:56 +0100

    add tslint, examples for routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import "../styles/index.css";
 
 import { NextPage } from "next";
+import Link from "next/link";
+import Header from "../components/header";
 
 const Home: NextPage = () => (
   <div className="p-4 shadow rounded bg-white">
+    <Header />
     <h1 className="text-purple-700 text-3xl ">Next.js</h1>
     <p className="text-gray-800">with Tailwind CSS and Typescript</p>
     <p className="text-gray-800">Made with ♥ by me at</p>
@@ -14,6 +17,12 @@ const Home: NextPage = () => (
     >
       https://github.com/xDecus
     </a>
+    <hr className="my-2" />
+    <Link href="/about">
+      <a className="text-purple-600 hover:text-purple-400">
+        About this template
+      </a>
+    </Link>
   </div>
 );
 
